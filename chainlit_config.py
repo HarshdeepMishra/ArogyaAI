ed1e418983c4a9f1e3161d3b944e96981ba9fb30
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2024-12-04 10:23:57 +0530

    Update chainlit_config.py

--- chainlit_config.py
+++ chainlit_config.py
@@ -1,14 +1,8 @@
 import chainlit as cl
 
-# Function to define settings when the chatbot starts
 @cl.on_start
 def on_start():
-    # Check the theme of the application (light or dark)
     if cl.theme == "dark":
-        # Set the logo for dark mode
         cl.set_logo("C:\MediMateAI\public\logo_dark.png")
     else:
-        # Set the logo for light mode
         cl.set_logo("C:\MediMateAI\public\logo_light.png")
-
-# Other custom configurations can be added here
